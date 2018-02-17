8d8183becf22554cb5aa1340fdc641c923f0cc81
Thomas.Legros <thomas.legros@embedit.cz>
2018-02-17 11:01:31 +0100

    base package

---
+++ src/main/java/com/github/t0mz06/cortana/CortanaApplication.java
@@ -0,0 +1,12 @@
+package com.github.t0mz06.cortana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CortanaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CortanaApplication.class, args);
+	}
+}
---
+++ src/test/java/com/github/t0mz06/cortana/CortanaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.github.t0mz06.cortana;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CortanaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
